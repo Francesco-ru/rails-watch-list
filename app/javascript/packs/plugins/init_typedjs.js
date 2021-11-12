30c3f14e615eeb5096983159ebc35cf51a91848f
francesco rulli <rulli.francesco1997@gmail.com>
2021-11-12 17:32:51 +0100

    remove js initi

--- app/javascript/packs/plugins/init_typedjs.js
+++ app/javascript/packs/plugins/init_typedjs.js
@@ -1,17 +1,1 @@
-import Typed from 'typed.js';
 
-const initTyped = () => {
-  const element = document.querySelector('.typed-js');
-  console.log(element);
-  if (element) {
-    new Typed('.typed-js', {
-      strings: ['Drama', 'Classic', 'Comedy', 'Action', 'Romantic', 'Thriller', 'Horror', 'Slasher'],
-      typeSpeed: 40,
-      loop: true,
-      loopCount: Infinity,
-      showCursor: false
-    });
-  }
-};
-
-export { initTyped };
